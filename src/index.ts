21e86bafa4bfc2579cf02dfa8b141f150f7cbb5c
Plamen Marinov <plamen_944@outlook.com>
2024-08-10 23:26:21 +0300

    Prepare package for publish

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,12 @@
-import { addConfigurationSectionKey } from "./adder";
-import { modifyConfigurationSectionKey } from "./modifier";
+import { addConfigurationSectionKey, addConfigurationSectionKeyByIndex } from "./adder";
+import { modifyConfigurationSectionKey, modifyConfigurationSectionKeyByIndex } from "./modifier";
 import { parseInitFromString } from "./parser";
-import { removeConfigurationSectionKey } from "./remover";
-import { Section } from "./types";
+import { removeConfigurationSectionKey, removeConfigurationSectionKeyByIndex } from "./remover";
+// import { Section, SectionEntry, Configuration } from "./types";
 
-const testString = `\
-[s]
-k = v`;
-
-
-const config = parseInitFromString(testString);
-for (var section of config.sections) {
-    console.log(`[${section.name}]`);
-
-    for (var entry of section.entries) {
-        console.log(`-> ${entry.key} = ${entry.value.replaceAll('\n', "\\n")}`);
-    }
+export {
+    parseInitFromString,
+    addConfigurationSectionKey, addConfigurationSectionKeyByIndex,
+    modifyConfigurationSectionKey, modifyConfigurationSectionKeyByIndex,
+    removeConfigurationSectionKey, removeConfigurationSectionKeyByIndex,
 }
-
-
-const newContent = removeConfigurationSectionKey(config, "s", "k");
-
-console.log("--------");
-console.log(newContent);
-console.log("--------");
--- tests/remover.test.ts
+++ tests/remover.test.ts
@@ -2,96 +2,96 @@ import { parseInitFromString } from "../src/parser";
 import { removeConfigurationSectionKey } from "../src/remover";
 
 describe('testing removing key', () => {
-        /*
-        * TEST
-        */
-        test('Remove key 1', () => {
-                const configStr = `\
+    /*
+    * TEST
+    */
+    test('Remove key 1', () => {
+        const configStr = `\
 [General]
 appName = SampleApp
 version = 1.0.0
 author = John Doe`;
 
-                const expectedStr = `\
+        const expectedStr = `\
 [General]
 appName = SampleApp
 author = John Doe`;
 
-                const config = parseInitFromString(configStr);
-                expect(removeConfigurationSectionKey(config, "General", "version")).toStrictEqual(expectedStr);
-        });
+        const config = parseInitFromString(configStr);
+        expect(removeConfigurationSectionKey(config, "General", "version")).toStrictEqual(expectedStr);
+    });
 
 
-        /*
-        * TEST
-        */
-        test('Remove key 2', () => {
-                const configStr = `\
+    /*
+    * TEST
+    */
+    test('Remove key 2', () => {
+        const configStr = `\
 [General]
 appName = SampleApp
 version = 1.0.0
 author = John Doe`;
 
-                const expectedStr = `\
+        const expectedStr = `\
 [General]
 appName = SampleApp
 version = 1.0.0
 `;
 
-                const config = parseInitFromString(configStr);
-                expect(removeConfigurationSectionKey(config, "General", "author")).toStrictEqual(expectedStr);
-        });
+        const config = parseInitFromString(configStr);
+        expect(removeConfigurationSectionKey(config, "General", "author")).toStrictEqual(expectedStr);
+    });
 
-        /*
-        * TEST
-        */
-        test('Remove key 3', () => {
-                const configStr = `\
+    /*
+    * TEST
+    */
+    test('Remove key 3', () => {
+        const configStr = `\
 [General]
 appName = SampleApp
 version = 1.0.0
 author = John Doe
 `;
 
-                const expectedStr = `\
+        const expectedStr = `\
 [General]
 appName = SampleApp
 version = 1.0.0
 `;
 
-                const config = parseInitFromString(configStr);
-                expect(removeConfigurationSectionKey(config, "General", "author")).toStrictEqual(expectedStr);
-        });
+        const config = parseInitFromString(configStr);
+        expect(removeConfigurationSectionKey(config, "General", "author")).toStrictEqual(expectedStr);
+    });
 
 
-        /*
-        * TEST
-        */
-        test('Remove key 4', () => {
-                const configStr = `\
+    /*
+    * TEST
+    */
+    test('Remove key 4', () => {
+        const configStr = `\
 [General]
     appName = SampleApp
     version = 1.0.0
     author = John Doe
 `;
 
-                const expectedStr = `\
+        const expectedStr = `\
 [General]
     appName = SampleApp
     version = 1.0.0
 `;
 
-                const config = parseInitFromString(configStr);
-                expect(removeConfigurationSectionKey(config, "General", "author")).toStrictEqual(expectedStr);
-        });
+        const config = parseInitFromString(configStr);
+        expect(removeConfigurationSectionKey(config, "General", "author")).toStrictEqual(expectedStr);
+    });
 
 
 
-        /*
-        * TEST
-        */
-        test('Remove key 5', () => {
-                const configStr = `\
+    /*
+    * TEST
+    */
+    test('Remove key 5', () => {
+        const configStr = `\
 [General]
     appName = SampleApp
 
@@ -103,7 +103,7 @@ version = 1.0.0
     author = John Doe
 `;
 
-                const expectedStr = `\
+        const expectedStr = `\
 [General]
     appName = SampleApp
 
@@ -111,8 +111,8 @@ version = 1.0.0
     author = John Doe
 `;
 
-                const config = parseInitFromString(configStr);
-                expect(removeConfigurationSectionKey(config, "General", "version")).toStrictEqual(expectedStr);
-        });
+        const config = parseInitFromString(configStr);
+        expect(removeConfigurationSectionKey(config, "General", "version")).toStrictEqual(expectedStr);
+    });
 });
 
